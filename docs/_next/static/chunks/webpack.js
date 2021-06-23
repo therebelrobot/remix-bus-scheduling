55c7f4a28e4dccb8fc994204cf3c19b267aa64c1
Aster Haven <github@therebelrobot.com>
2021-06-23 11:47:54 -0700

    updating docs build

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c0f8d8077bad938a5f46";
+/******/ 	var hotCurrentHash = "93733c48715e28364040";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
